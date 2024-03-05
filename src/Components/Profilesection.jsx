3175f13d09ad956a3afed5bd4686cbfdd414f35f
rcout328 <solovpxoffical@gmail.com>
2024-03-06 01:00:28 +0530

    c

--- src/Components/Profilesection.jsx
+++ src/Components/Profilesection.jsx
@@ -70,76 +70,76 @@ const Profilesection = () => {
 
   return (
     <>
-    <div>
-      {user ? (
-        <>
-          <div className="flex flex-col h-screen justify-center items-center bg-[#141414] phone:flex phone:flex-wrap phone:p-4 ">
-            <h1 className="text-4xl mb-4 font-medium text-white phone:text-3xl phone:font-medium">
-              Whos watching?
-            </h1>
-            <div className="flex flex-row justify-center items-center gap-8 phone:flex phone:flex-wrap ">
-              <div className="flex flex-row justify-center items-center gap-8  ">
-                <animated.div
-                  style={fadeAnimation}
-                  className="flex flex-row gap-8 phone:gap-3 justify-center items-center phone:flex phone:flex-wrap "
-                >
-                  {datas.map(
-                    (data) =>
-                      currentUserUid === data.uid && (
-                        <div
-                          key={data.id}
-                          className="flex flex-col items-center justify-center mt-5 phone:flex phone:flex-wrap"
-                        >
-                          <div onClick={() => handleProfileClick(data.id)}>
-                            <Link to="/home">
-                              <img
-                                src={data.image}
-                                alt={`Profile - ${data.pname}`}
-                                className="w-40 h-40 object-cover rounded-lg phone:w-30 phone:h-30 "
-                              />
-                            </Link>
-                          </div>
+      <div>
+        {user ? (
+          <>
+            <div className="flex flex-col h-screen justify-center items-center bg-[#141414] phone:flex phone:flex-wrap phone:p-4 ">
+              <h1 className="text-4xl mb-4 font-medium text-white phone:text-3xl phone:font-medium">
+                Whos watching?
+              </h1>
+              <div className="flex flex-row justify-center items-center gap-8 phone:flex phone:flex-wrap ">
+                <div className="flex flex-row justify-center items-center gap-8  ">
+                  <animated.div
+                    style={fadeAnimation}
+                    className="flex flex-row gap-8 phone:gap-3 justify-center items-center phone:flex phone:flex-wrap "
+                  >
+                    {datas.map(
+                      (data) =>
+                        currentUserUid === data.uid && (
+                          <div
+                            key={data.id}
+                            className="flex flex-col items-center justify-center mt-5 phone:flex phone:flex-wrap"
+                          >
+                            <div onClick={() => handleProfileClick(data.id)}>
+                              <Link to="/home">
+                                <img
+                                  src={data.image}
+                                  alt={`Profile - ${data.pname}`}
+                                  className="w-40 h-40 object-cover rounded-lg phone:w-30 phone:h-30 "
+                                />
+                              </Link>
+                            </div>
 
-                          <h1 className="font-medium mt-2 text-white/50">
-                            {data.pname}
-                          </h1>
-                          {show && (
-                            <Link to={`/manageprof/${data.id}`}>
-                              <button
-                                className="text-red-600"
-                                onClick={() => handleUpdateClick(data.id)}
-                              >
-                                Update
-                              </button>
-                            </Link>
-                          )}
-                        </div>
-                      )
-                  )}
-                </animated.div>
+                            <h1 className="font-medium mt-2 text-white/50">
+                              {data.pname}
+                            </h1>
+                            {show && (
+                              <Link to={`/manageprof/${data.id}`}>
+                                <button
+                                  className="text-red-600"
+                                  onClick={() => handleUpdateClick(data.id)}
+                                >
+                                  Update
+                                </button>
+                              </Link>
+                            )}
+                          </div>
+                        )
+                    )}
+                  </animated.div>
+                </div>
+                <button className="text-5xl font-bold text-white">
+                  <Link to="/padd">Add</Link>
+                </button>
               </div>
-              <button className="text-5xl font-bold text-white">
-                <Link to="/padd">Add</Link>
+              <button
+                className="bg-white p-4 mt-20"
+                onClick={() => setShow(!show)}
+              >
+                Manage Profile
               </button>
             </div>
-            <button
-              className="bg-white p-4 mt-20"
-              onClick={() => setShow(!show)}
-            >
-              Manage Profile
+          </>
+        ) : (
+          <div className="flex flex-col justify-center items-center font-bold text-5xl inset-0 absolute text-[#E50914]">
+            Please Login to Access this Page
+            <button className="text-xl bg-[#E50914] text-white w-20 mt-10 p-4 flex justify-center items-center rounded-full">
+              <Link to="/login">Login</Link>
             </button>
           </div>
-        </>
-      ) : (
-        <div className="flex flex-col justify-center items-center font-bold text-5xl inset-0 absolute text-[#E50914]">
-          Please Login to Access this Page
-          <button className="text-xl bg-[#E50914] text-white w-20 mt-10 p-4 flex justify-center items-center rounded-full">
-            <Link to="/login">Login</Link>
-          </button>
-        </div>
-      )}
+        )}
+      </div>
     </>
-    </div>
   );
 };
 
